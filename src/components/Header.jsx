9773505685ce7cbc6574ef8c8e4416b0e0cebf69
rrt78537-dotcom <rrt78537@gmail.com>
2026-02-26 10:15:05 +0500

    manu ended

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import rasm from "../../public/img/Logo.svg"
+
+function Header() {
+  return (
+    <div className="fixed top-0 left-0 right-0 bg-white/30 backdrop-blur shadow-lg/10">
+        <nav className="flex justify-between max-w-7xl mx-auto items-center py-6 ">
+        <img src={rasm} alt="" />
+        <div>
+          <button className="mr-8 font-semibold p-2 text-[#4B2995] bg-[#EBE5F9] rounded-lg">
+            <i class="fa-solid fa-location-dot"></i>
+            Porto Alegre, RS
+          </button>
+          <button className="p-2 text-[#C47F17] bg-[#F1E9C9] rounded-lg">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,51 +1,69 @@
-import React from "react";
-import rasm from "../../public/img/Logo.svg"
+import Header from "../components/Header";
+import banner from "../../public/img/Imagem.svg"
+import img from "../../public/img/Image.svg"
+import backdrop from "../../public/img/Background.svg"
 
 function Menu() {
   return (
     <div>
-      <nav className="flex justify-between max-w-7xl mx-auto items-center my-6">
-        <img src={rasm} alt="" />
+      <div className="mb-40">
+        <Header/>
+      </div>
+      <div className="max-w-7xl mx-auto flex justify-around mb-25 bg-{backdrop}">
         <div>
-          <button className="mr-8 font-semibold p-2 text-[#4B2995] bg-[#EBE5F9] rounded-lg">
-            <i class="fa-solid fa-location-dot"></i>
-            Porto Alegre, RS
-          </button>
-          <button className="p-2 text-[#C47F17] bg-[#F1E9C9] rounded-lg">
-            <i class="fa-solid fa-cart-shopping"></i>
-          </button>
-        </div>
-      </nav>
-
-      <div className="max-w-7xl mx-auto">
-        <div>
-            <h1 className="font-mono">Encontre o café perfeito <br /> para qualquer hora do dia</h1>
-            <p>Com o Coffee Delivery você recebe seu café onde estiver, a <br /> qualquer hora</p>
-            <div className="flex">
+            <h1 className="text-5xl font-bold font-mono">Encontre o café perfeito <br /> para qualquer hora do dia</h1>
+            <p className="text-2xl font-medium lg:my-6">Com o Coffee Delivery você recebe seu café onde estiver, a <br /> qualquer hora</p>
+            <div className="flex lg:mt-15">
                 <div className="mr-4">
                     <div className="flex gap-3 items-center mb-4">
-                        <button className="bg-[#C47F17] text-white rounded-full p-2"><i class="fa-solid fa-cart-shopping"></i></button>
-                        <h1>Compra simples e segura</h1>
+                        <button className="px-2.5 bg-[#C47F17] text-white rounded-full p-2"><i class="fa-solid fa-cart-shopping"></i></button>
+                        <h1 className="text-lg">Compra simples e segura</h1>
                     </div>
                     <div className="flex gap-3 items-center">
-                        <button className="bg-[#DBAC2C] text-white rounded-full p-2"><i class="fa-solid fa-stopwatch"></i></button>
-                        <h1>Entrega rápida e rastreada</h1>
-                    </div>
+                        <button className="px-2.5 bg-[#DBAC2C] text-white rounded-full p-2"><i class="fa-solid fa-stopwatch"></i></button>
+                        <h1 className="text-lg">Entrega rápida e rastreada</h1>
+                    </div>  
                 </div>
                 <div>
                     <div className="flex gap-3 items-center mb-4">
-                        <button className="bg-[#574F4D] text-white rounded-full p-2"><i class="fa-solid fa-box-open"></i></button>
-                        <h1>Embalagem mantém o café intacto</h1>
+                        <button className="px-2.5 bg-[#574F4D] text-white rounded-full p-2"><i class="fa-solid fa-box-open"></i></button>
+                        <h1 className="text-lg">Embalagem mantém o café intacto</h1>
                     </div>
                     <div className="flex gap-3 items-center">
-                        <button className="bg-[#8047F8] text-white rounded-full p-2"><i class="fa-solid fa-mug-hot"></i></button>
-                        <h1>O café chega fresquinho até você</h1>
+                        <button className="px-2.5 bg-[#8047F8] text-white rounded-full p-2"><i class="fa-solid fa-mug-hot"></i></button>
+                        <h1 className="text-lg">O café chega fresquinho até você</h1>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div></div>
+        <div>
+          <img src={banner} alt="" />
+        </div>
+      </div>
+
+      <div className="lg:max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold font-mono mb-8">Nossos cafés</h1>
+        <div className="lg:grid grid-cols-4 gap-8 my-10">
+          <div className="bg-[#F3F2F2] text-center px-4 pb-4 rounded-bl-4xl rounded-tr-4xl transition-all duration-700 hover:rounded-tr-[220px] hover:shadow-[16px_32px_50px_-4px_#000000]">
+          <div className="flex justify-center">
+            <img src={img} alt="" />
+          </div>
+          <button className="p-1 font-bold rounded-xl my-4 bg-[#F1E9C9] text-[#C47F17]">Tradicional</button>
+          <h1 className="text-2xl font-bold font-mono">Expresso Tradicional</h1>
+          <p className="text-[#8D8686] font-medium text-md mt-4">O tradicional café feito com água <br /> quente e grãos moídos</p>
+          <div className="flex gap-6 items-center justify-between mt-8">
+            <h1 className="font-medium font-mono">R$ <b className="text-xl">9,90</b></h1>
+            <div className="font-bold text-lg flex gap-5 px-4 p-2 rounded-lg bg-[#E6E5E5]">
+              <p className="text-[#8047F8]">-</p>
+              <p>1</p>
+              <p className="text-[#8047F8]">+</p>
+            </div>
+            <button className="rounded-lg p-2 bg-[#4B2995] text-white"><i class="fa-solid fa-cart-shopping"></i></button>
+          </div>
+        </div>
+
+        </div>
       </div>
     </div>
   );
