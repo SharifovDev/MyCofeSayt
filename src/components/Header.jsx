b96bd9230e0e4e63a2affadd4f6c8f252e5a163d
rrt78537-dotcom <rrt78537@gmail.com>
2026-02-26 15:04:39 +0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import rasm from "../../public/img/Logo.svg"
 function Header() {
   return (
     <div className="fixed top-0 left-0 right-0 bg-white/30 backdrop-blur shadow-lg/10">
-        <nav className="flex justify-between max-w-7xl mx-auto items-center py-6 ">
+        <nav className="flex justify-between lg:max-w-7xl md:max-w-2xl max-w-xs mx-auto items-center py-6 ">
         <img src={rasm} alt="" />
         <div>
-          <button className="mr-8 font-semibold p-2 text-[#4B2995] bg-[#EBE5F9] rounded-lg">
+          <button className="md:mr-8 mr-2 font-semibold p-2 text-[#4B2995] bg-[#EBE5F9] rounded-lg">
             <i class="fa-solid fa-location-dot"></i>
             Porto Alegre, RS
           </button>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import banner from "../../public/img/Imagem.svg"
 import img from "../../public/img/Image.svg"
-import backdrop from "../../public/img/Background.svg"
 
 function Menu() {
   return (
@@ -9,12 +8,12 @@ function Menu() {
       <div className="mb-40">
         <Header/>
       </div>
-      <div className="max-w-7xl mx-auto flex justify-around mb-25 bg-{backdrop}">
+      <div className="lg:max-w-7xl md:max-w-2xl max-w-2xs mx-auto flex flex-wrap md:flex-nowrap justify-around mb-25 bg-{backdrop}">
         <div>
-            <h1 className="text-5xl font-bold font-mono">Encontre o café perfeito <br /> para qualquer hora do dia</h1>
-            <p className="text-2xl font-medium lg:my-6">Com o Coffee Delivery você recebe seu café onde estiver, a <br /> qualquer hora</p>
+            <h1 className="lg:text-5xl md:text-2xl text-xl font-bold font-mono">Encontre o café perfeito <br /> para qualquer hora do dia</h1>
+            <p className="lg:text-2xl hidden lg:block font-medium lg:my-6">Com o Coffee Delivery você recebe seu café onde estiver, a <br /> qualquer hora</p>
             <div className="flex lg:mt-15">
-                <div className="mr-4">
+                <div className="mr-4 hidden lg:block">
                     <div className="flex gap-3 items-center mb-4">
                         <button className="px-2.5 bg-[#C47F17] text-white rounded-full p-2"><i class="fa-solid fa-cart-shopping"></i></button>
                         <h1 className="text-lg">Compra simples e segura</h1>
@@ -24,14 +23,14 @@ function Menu() {
                         <h1 className="text-lg">Entrega rápida e rastreada</h1>
                     </div>  
                 </div>
-                <div>
+                <div className="mt-10 lg:mt-0">
                     <div className="flex gap-3 items-center mb-4">
                         <button className="px-2.5 bg-[#574F4D] text-white rounded-full p-2"><i class="fa-solid fa-box-open"></i></button>
-                        <h1 className="text-lg">Embalagem mantém o café intacto</h1>
+                        <h1 className="md:text-lg">Embalagem mantém o café intacto</h1>
                     </div>
                     <div className="flex gap-3 items-center">
                         <button className="px-2.5 bg-[#8047F8] text-white rounded-full p-2"><i class="fa-solid fa-mug-hot"></i></button>
-                        <h1 className="text-lg">O café chega fresquinho até você</h1>
+                        <h1 className="md:text-lg">O café chega fresquinho até você</h1>
                     </div>
                 </div>
             </div>
@@ -42,27 +41,26 @@ function Menu() {
         </div>
       </div>
 
-      <div className="lg:max-w-7xl mx-auto">
+      <div className="lg:max-w-7xl md:max-w-2xl max-w-2xs mx-auto">
         <h1 className="text-4xl font-bold font-mono mb-8">Nossos cafés</h1>
-        <div className="lg:grid grid-cols-4 gap-8 my-10">
-          <div className="bg-[#F3F2F2] text-center px-4 pb-4 rounded-bl-4xl rounded-tr-4xl transition-all duration-700 hover:rounded-tr-[220px] hover:shadow-[16px_32px_50px_-4px_#000000]">
+        <div className="lg:grid grid-cols-4 gap-8 flex flex-wrap gap-8 my-10">
+          <div className="bg-gray-300 text-center px-4 pb-4 rounded-bl-4xl rounded-tr-4xl transition-all duration-700 hover:rounded-tr-[220px] hover:shadow-[16px_32px_50px_-4px_#000000]">
           <div className="flex justify-center">
-            <img src={img} alt="" />
+            <img src={img}/>
           </div>
           <button className="p-1 font-bold rounded-xl my-4 bg-[#F1E9C9] text-[#C47F17]">Tradicional</button>
           <h1 className="text-2xl font-bold font-mono">Expresso Tradicional</h1>
           <p className="text-[#8D8686] font-medium text-md mt-4">O tradicional café feito com água <br /> quente e grãos moídos</p>
           <div className="flex gap-6 items-center justify-between mt-8">
             <h1 className="font-medium font-mono">R$ <b className="text-xl">9,90</b></h1>
-            <div className="font-bold text-lg flex gap-5 px-4 p-2 rounded-lg bg-[#E6E5E5]">
+            <div className="font-bold text-xl flex gap-5 px-4 p-2 rounded-lg bg-[#E6E5E5]">
               <p className="text-[#8047F8]">-</p>
               <p>1</p>
               <p className="text-[#8047F8]">+</p>
             </div>
             <button className="rounded-lg p-2 bg-[#4B2995] text-white"><i class="fa-solid fa-cart-shopping"></i></button>
           </div>
-        </div>
-
+          </div>
         </div>
       </div>
     </div>
