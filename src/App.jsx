2ff42107bcd57dbc45694d2e8d3605275aae24a2
rrt78537-dotcom <rrt78537@gmail.com>
2026-02-28 15:08:12 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Menu from './pages/Menu'
 import Admin from './pages/Admin'
 import Orders from './pages/Orders'
 import Login from './components/Login'
+import Location from './pages/Location'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/admin12345' element={<Admin/>}/>
         <Route path='/order' element={<Orders/>}/>
         <Route path='/admin' element={<Login/>}/>
+        <Route path='/location' element={<Location/>}/>
       </Routes>
   )
 }
---
+++ src/pages/Location.jsx
@@ -0,0 +1,22 @@
+import Header from "../components/Header"
+
+function Location() {
+  return (
+    <div>
+        <Header/>
+        <div>
+            <div>
+                <h1>Complete seu pedido</h1>
+                <div>
+                    <h1>Endereço de Entrega</h1>
+                    <p>Informe o endereço onde deseja receber seu pedido</p>
+                </div>
+            </div>
+
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Location
