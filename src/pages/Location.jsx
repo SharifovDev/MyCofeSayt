5bf85fbf32b75c856202181c81a6d76854fb5ea5
rrt78537-dotcom <rrt78537@gmail.com>
2026-03-02 11:18:43 +0500

    location page started

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,17 +1,18 @@
 import Header from "../components/Header";
 
 function Location() {
+    
   return (
     <div>
       <div className="mb-30">
         <Header />
       </div>
-      <div className="max-w-7xl mx-auto flex justify-between">
+      <div className="max-w-7xl mx-auto flex justify-around">
         <div>
           <h1 className="p-4 text-2xl font-bold font-mono">
             Complete seu pedido
           </h1>
-          <div className="grid gap-6 border-2 rounded-lg p-6">
+          <div className="grid gap-6 rounded-xl p-10 bg-[#F3F2F2]">
             <div className="my-3">
               <h1 className="text-lg font-medium mb-1">
                 📍 Endereço de Entrega
@@ -32,7 +33,7 @@ function Location() {
             />
             <input
               className="border-2 rounded-lg p-2 font-bold"
-              type="text"
+              type="number"
               placeholder="Number"
             />
           </div>
@@ -42,18 +43,38 @@ function Location() {
           <h1 className="p-4 text-2xl font-bold font-mono">
             Cafés selecionados
           </h1>
-          <div className="border-2 rounded-lg p-6">
-            <div>
-              <div className="flex justify-between">
+          <div className="p-8 rounded-bl-3xl rounded-tr-3xl bg-[#F3F2F2]">
+            <div className="flex my-4">
+              <div>
+                <img src=""/>
+              </div>
+              <div>
+                <div className="flex justify-between text-lg mb-3" >
+                  <h1>Expresso Tradicional</h1>
+                  <p className="font-bold">R$ 9,90</p>
+                </div>
+                <div className="grid grid-cols-3 gap-2">
+                  <div className="font-bold text-xl flex gap-5 text-center p-2 pl-5 rounded-lg bg-[#E6E5E5]">
+                    <p className="text-[#8047F8]">-</p>
+                    <p>0</p>
+                    <p className="text-[#8047F8]">+</p>
+                  </div> 
+                  <button className="font-semibold text-lg text-center p-2 rounded-lg bg-[#E6E5E5]">🗑 Remover</button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className="my-4">
+              <div className="flex justify-between text-lg mb-2">
                 <h1>Total de itens</h1>
                 <p>R$ 29,70</p>
               </div>
-              <div className="flex justify-between">
+              <div className="flex justify-between text-xl font-bold">
                 <h1>Total</h1>
                 <p>R$ 29,70</p>
               </div>
             </div>
-            <button className="bg-[#DBAC2C] rounded-lg p-2 font-semibold text-xl">
+            <button className="bg-[#DBAC2C] rounded-lg p-2 font-semibold text-lg text-white px-10 mx-18 mt-4">
               confirmar pedido
             </button>
           </div>
